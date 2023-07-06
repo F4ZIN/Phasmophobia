81f68e0db48169b43146702f8e6bf12495e1f342
Zach L <109915621+F4Gopic@users.noreply.github.com>
2023-07-05 22:09:13 -0400

    Update

---
+++ Source/Phasmophobia/PhasmophobiaGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PhasmophobiaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PHASMOPHOBIA_API APhasmophobiaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
